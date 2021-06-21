caa9c3854a7c2458a465dc963bf2e54d7f4dd65c
aspirin <mhb7724@qq.com>
2021-06-21 12:03:20 +0800

    :lipstick: polish

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Highboo52na'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Highboo52na'
 
 export default function Layout({ children, home }) {
   return (
